85cb20c417b774bc68c598390622027398b58f96
vqkhanh2009@gmail.com <vqkhanh2009@gmail.com>
2022-08-15 15:24:35 +0700

    71. Tasks and Users - Database Relation

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,5 +1,6 @@
 import { TaskStatus } from './task-status.enum';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/auth/user.entity';
 
 @Entity()
 export class Task {
@@ -14,4 +15,7 @@ export class Task {
 
   @Column()
   status: TaskStatus;
+
+  @ManyToOne((_type) => User, (user) => user.task, { eager: false })
+  user: User;
 }
