3abe93ad91ba42f8e9138253da1d6add19d6f09f
vqkhanh2009@gmail.com <vqkhanh2009@gmail.com>
2022-08-16 10:35:07 +0700

    revert draf code

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,4 +1,3 @@
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
@@ -7,11 +6,7 @@ import { TasksRepository } from './tasks.repository';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [
-    ConfigModule,
-    TypeOrmModule.forFeature([TasksRepository]),
-    AuthModule,
-  ],
+  imports: [TypeOrmModule.forFeature([TasksRepository]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
